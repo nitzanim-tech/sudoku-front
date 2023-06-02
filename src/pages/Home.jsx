e8dd80e1a27639e5098f075e3e1cd944d1e16658
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-02 21:27:36 +0300

    sumbit added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { generateSudoku } from "../util/generateSudoku";
-import { runPython } from "../util/RunPython";
-
 import SudokuTable from "../components/SudukuTable";
 
 function Home() {
   const [count, setCount] = useState(0);
   const board = generateSudoku();
   console.log(board);
-  const answer = runPython("C:Users/hagbi/Desktop/pythonProject/combined.py", [
-    [None, 3, None, 1],
-    [2, 1, 4, 3],
-    [3, 2, 1, None],
-    [1, 4, None, None],
-  ]);
-  console.log(answer);
+  const answer = fetch("/run-script", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ script, input }),
+  })
+    .then((response) => response.text())
+    .then((output) => {
+      console.log(output);
+    });
+
 
   return (
     <>
