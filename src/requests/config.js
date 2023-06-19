6129f8f87aa3035ff9a5a5c06ed107a0b9bc2d40
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-19 19:31:30 +0300

    update url

--- src/requests/config.js
+++ src/requests/config.js
@@ -1,8 +1,8 @@
 const config = {
-  url: "https://194e-34-91-78-77.ngrok-free.app/",
+  url: "https://3431-34-91-78-77.ngrok-free.app/",
   header: {
     "Content-Type": "application/json",
-    "Bypass-Tunnel-Reminder": "test",
+    "ngrok-skip-browser-warning": "true",
   },
 };
 export default config;
