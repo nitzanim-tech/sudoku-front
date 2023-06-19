e0d7775c14e1a45183c69b3289d77a3529826eed
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-19 18:27:23 +0300

    update confing

--- src/requests/config.js
+++ src/requests/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "https://nine-apples-sleep.loca.lt/",
+  url: "https://194e-34-91-78-77.ngrok-free.app/",
   header: {
     "Content-Type": "application/json",
     "Bypass-Tunnel-Reminder": "test",
